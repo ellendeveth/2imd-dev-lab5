d46c0c510ca6860b8fcac47b4c94fa1d2e9d6cc5
Ellen <ellen.deveth@hotmail.com>
2022-03-17 22:31:50 +0100

    fix: fix port bug

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const messagesRouter = require("./routers/messages");
 
 mongoose.connect('mongodb://localhost:27017/chatapp');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000
 app.set('view engine', 'pug');
 
 app.use(express.json()); //json herkennen en bijvoegen op body
