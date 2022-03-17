96059de6542589d7d2667a90eab7b85afa86ae0d
Ellen <ellen.deveth@hotmail.com>
2022-03-17 19:45:54 +0100

    feat: add routers

--- routers/messages.js
+++ routers/messages.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const messagesController = require("./../controllers/messages.js");
 
 router.get('/', messagesController.getAll); //functie binnen controller
+router.get('/:id', messagesController.getId);
+
 router.post('/', messagesController.create);
+
+router.put('/:id', messagesController.update);
+router.delete('/:id', messagesController.del);
 
 module.exports = router;
